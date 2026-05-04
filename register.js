18daf869f89329a197905abc66cf41ab9bceab57
N34FOUR <tickletimemyboi@gmail.com>
2026-05-03 19:32:24 -0500

    remove exposed token and fix auth handling

--- register.js
+++ register.js
@@ -3,15 +3,31 @@ const { REST, Routes, SlashCommandBuilder } = require("discord.js");
 const commands = [
   new SlashCommandBuilder()
     .setName("ping")
-    .setDescription("Test if the bot is alive")
+    .setDescription("Test bot"),
+
+  new SlashCommandBuilder()
+    .setName("link")
+    .setDescription("Link your Epic Games account")
+    .addStringOption(option =>
+      option.setName("epic")
+        .setDescription("Your Epic username")
+        .setRequired(true)
+    )
 ].map(cmd => cmd.toJSON());
 
 const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 (async () => {
-  await rest.put(
-    Routes.applicationCommands("1500616659910656020"),
-    { body: commands }
-  );
-  console.log("Commands registered");
+  try {
+    console.log("Registering commands...");
+
+    await rest.put(
+      Routes.applicationCommands("1500616659910656020"),
+      { body: commands }
+    );
+
+    console.log("Commands registered successfully!");
+  } catch (error) {
+    console.error(error);
+  }
 })();
